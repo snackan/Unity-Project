62c130207c7607dc18d9b177f688b52b6c327bbb
SebastianPogchamp <sebastian.nordin@elev.ga.lbs.se>
2022-01-20 14:08:58 +0100

    Fixade menyer

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
   public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Modified Minds");
     }
 
     public void QuitGame()
