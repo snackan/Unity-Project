8a970d26130a8e674f6ed14eca113dc268c9d6a8
Mickman321 <97745994+Mickman321@users.noreply.github.com>
2022-01-28 11:43:23 +0100

    new dash

--- Assets/DashMove.cs
+++ Assets/DashMove.cs
@@ -11,6 +11,8 @@ public class DashMove : MonoBehaviour
     public float startDashTime;
     private int direction;
 
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,50 +28,64 @@ public class DashMove : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
                 direction = 1;
+                dashtTime = startDashTime;
             }
             else if (Input.GetKeyDown(KeyCode.RightArrow))
             {
                 direction = 2;
+                dashtTime = startDashTime;
             }
             else if (Input.GetKeyDown(KeyCode.UpArrow))
             {
                 direction = 3;
+                dashtTime = startDashTime;
             }
             else if (Input.GetKeyDown(KeyCode.DownArrow))
             {
                 direction = 4;
-            }
-            else
-            {
-                if(dashtTime <= 0)
-                {
-                    direction = 0;
-                    dashtTime = startDashTime;
-                    rb.velocity = Vector2.zero;
-                }
-                else
-                {
-                    dashtTime -= Time.deltaTime;
-
-                    if (direction == 1)
-                    {
-                        rb.velocity = Vector2.left * dashSpeed;
-                    }
-                    else if (direction == 2)
-                    {
-                        rb.velocity = Vector2.right * dashSpeed;
-                    }
-                    else if (direction == 3)
-                    {
-                        rb.velocity = Vector2.up * dashSpeed;
-                    }
-                    else if (direction == 4)
-                    {
-                        rb.velocity = Vector2.down * dashSpeed;
-                    }
-                }
+                dashtTime = startDashTime;
             }
 
         }
+        if (dashtTime <= 0)
+        {
+            direction = 0;
+            dashtTime = startDashTime;
+            //rb.velocity = Vector2.zero;
+        }
+        else
+        {
+            dashtTime -= Time.deltaTime;
+
+            if (direction == 1)
+            {
+                rb.velocity = Vector2.left * dashSpeed;
+            }
+            else if (direction == 2)
+            {
+                rb.velocity = Vector2.right * dashSpeed;
+            }
+            else if (direction == 3)
+            {
+                rb.velocity = Vector2.up * dashSpeed;
+            }
+            else if (direction == 4)
+            {
+                rb.velocity = Vector2.down * dashSpeed;
+            }
+        }
+
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 5;
+          mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 difference = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            rb.velocity = -difference.normalized * speed;
+        }
+
+        
     }
 }
